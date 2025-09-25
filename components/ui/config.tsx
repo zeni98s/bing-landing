86ad3b41c6585a76ce4885cd23fc80938acb7e4a
zeni jaelani <tetrischain208@gmail.com>
2025-09-25 22:55:07 +0700

    Update config.tsx

--- components/ui/config.tsx
+++ components/ui/config.tsx
@@ -6,9 +6,9 @@ export const CONFIG = {
   chain: "Solana",
   contract: "Coming Soon", // ganti dengan real address nanti
   urls: {
-    dexscreener: "https://dexscreener.com/solana/",
-    trade: "https://gmgn.ai/",
-    pumpfun: "https://pump.fun/coin/",
+    dexscreener: "https://dexscreener.com/solana/ap3tgbiulojtdvz2arrfltg2jc8qnh9xadcupkavfmpd",
+    trade: "https://gmgn.ai/sol/token/69ncXHPhKxaqfp9MJ5zLMZ8EJx3EmjeNpEy857adpump",
+    pumpfun: "https://pump.fun/board?q=69ncXHPhKxaqfp9MJ5zLMZ8EJx3EmjeNpEy857adpump",
     telegram: "https://t.me/+izMOvNT-4bdmNjNl",
     twitter: "https://x.com/Bing_onBONK",
     buy: "https://bonk.fun/",
