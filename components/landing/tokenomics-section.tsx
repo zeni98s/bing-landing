b39a87ff947de3fc2d4feb86dd3cd4effec4727b
zeni jaelani <tetrischain208@gmail.com>
2025-09-25 17:46:09 +0700

    Update tokenomics-section.tsx

--- components/landing/tokenomics-section.tsx
+++ components/landing/tokenomics-section.tsx
@@ -93,7 +93,7 @@ export default function TokenomicsSection() {
                 </button>
               </Link>
               <Link
-                href={CONFIG.urls.dexscreener}
+                href={CONFIG.urls.buy}
                 target="_blank"
                 rel="noopener noreferrer"
               >
